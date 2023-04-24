5d999c7d67fa39a594c9df6f3197392670af048d
AdibFuse <adib@letsfuse.com>
2023-04-24 22:50:56 +0100

    Remove handlers requiring access token

--- src/fuse.controller.ts
+++ src/fuse.controller.ts
@@ -33,31 +33,6 @@ export class FuseController {
             res.status(200).json(await this.service.exchangePublicToken(public_token));
         });
 
-        this.router.get('/accounts', async (req, res) => {
-            const { access_token } = req.body;
-            res.status(200).json(await this.service.getAccounts(access_token));
-        });
-
-        this.router.get('/account-details', async (req, res) => {
-            const { access_token, remote_account_ids } = req.body;
-            res.status(200).json(await this.service.getAccountDetails(access_token));
-        });
-
-        this.router.get('/balances', async (req, res) => {
-            const { access_token, remote_account_ids } = req.body;
-            res.status(200).json(await this.service.getBalances(access_token, remote_account_ids));
-        });
-
-        this.router.get('/owners', async (req, res) => {
-            const { access_token } = req.body;
-            res.status(200).json(await this.service.getOwners(access_token));
-        });
-
-        this.router.get('/transactions', async (req, res) => {
-            const { access_token } = req.body;
-            res.status(200).json(await this.service.getTransactions(access_token));
-        });
-
         this.router.post('/webhooks', async (req, res) => {
             const { status } = await this.service.handleWebhook(req.headers, req.body);
             res.status(status);
